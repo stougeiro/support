ea555f0b7de2103d3c778cb4ab9d1a0bf9bad41a
STougeiro <sidneytougeiro@gmail.com>
2024-10-03 11:05:44 -0300

    Include aliases support

---
+++ src/aliases.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+    use STDW\Support\Arr;
+    use STDW\Support\Dir;
+    use STDW\Support\File;
+    use STDW\Support\Password;
+    use STDW\Support\SafeBase64;
+    use STDW\Support\Str;
+    use STDW\Support\TTL;
+
+
+    class_alias(Arr::class, 'Arr');
+    class_alias(Dir::class, 'Dir');
+    class_alias(File::class, 'File');
+    class_alias(Password::class, 'Password');
+    class_alias(SafeBase64::class, 'SafeBase64');
+    class_alias(Str::class, 'Str');
+    class_alias(TTL::class, 'TTL');
